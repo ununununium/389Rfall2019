a1a2bdd73ce37e7dd904743bca0c51db48755366
ununununium <izhongyuting@gmail.com>
2019-10-17 02:18:51 -0400

    almost

--- week/7/brutefore.py
+++ week/7/brutefore.py
@@ -4,51 +4,34 @@ import operator
 import threading
 import subprocess
 import time
+import os
+
 
 wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
 
-pw_find = False
 
-def create_threads(t, passwords):
-	n = len(passwords)
-	x = n // t
-	m = n % t
-	xs = [passwords[i:i+x] for i in range(0, n, x)]
-	if m:
-		xs[t-1:] = [passwords[-m-x:]]
-	assert(sum([len(l) for l in xs]) == n)
-	return [threading.Thread(target=run_brute_force, args=(l)) for l in xs]
 
-def run_brute_force(*passwords):
-	global pw_find
-
-	for pw in passwords:
-		if pw_find:
-			break
-		else:
-			if brute_force(pw.rstrip()):
-				pw_find = True
 
 def brute_force(pw):
-	cmd = "steghide extract -sf image_dup -xf out.txt"
-	returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
-	time.sleep(0.5)
-	res = subprocess.call(pw,shell=True)
-	if "could not extract any data with that passphrase" in res:
-		return False
-	else:
-		return True
+	cmd = "steghide extract -sf image_dup -xf out.txt -p " + pw
+
+
+	# result = subprocess.run(cmd, stdout=subprocess.PIPE)  # returns the exit code in unix
+	# print(result.stdout)
+	# if "could not extract any data with that passphrase" in str(returned_value):
+	# 	return False
+	# else:
+	# 	return True
 
 
 if __name__ == '__main__':
 	file = open(wordlist,"r")
 	passwords = file.readlines()
-	thread_list = create_threads(100,passwords)
 
-	for thread in thread_list:
-		print('[*] Running thread: {}.'.format(thread.getName()))
-		thread.start()
-
-	for thread in thread_list:
-		print('[*] Wating for {} to join.'.format(thread.getName()))
-		thread.join()
+	for pw in passwords:
+		print("Trying: " + str(pw))
+		brute_force(pw)
+		break
+		# if brute_force(pw):
+		# 	print("PASSWORD GET: " + pw )
+		# 	break
